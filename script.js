1f066715a39cf7e6d1dfc56076c93b423ba5462b
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-01-19 16:09:15 -0500

    js commit3

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Password Generator Returns - https://net-comber.com/charset.html
-
 function generatePassword() {
   var newPassword = "";
   var characters = parseInt(
@@ -24,32 +22,45 @@ function generatePassword() {
     "Click OK to confirm including special characters in your password."
   );
 
-  //asks user how many characters to generate for password & shows user length
-  //sets input field criteria between 8 and 128 characters in length & gives alert if violated
-  if (characters < 8 || characters > 128 || characters == null) {
+  //Asks user how many characters to generate for password & shows user length
+  //Sets input field criteria between 8 and 128 characters in length & gives alert if violated
+  if (
+    characters < 8 ||
+    characters > 128 ||
+    characters == null
+    // || characters == false
+  ) {
     alert("Please enter character length between 8 and 128.");
   }
   //Looping until chosen characters length has been met
   for (var i = 0; i < characters; i++) {
-    // Displays generated password to user out of 26 lowercase Browser letters
-    // Concatenates newPassword variable with chosen conditions
+    //Password Generator Returns - https://net-comber.com/charset.html
+    //Chooses random password out of 26 lowercase Browser letters
+    //Concatenates newPassword variable with chosen conditions
     if (lowercase == true) {
       newPassword += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
     }
-
-    // Displays generated password to user out of 26 uppercase Browser letters
 
     if (uppercase == true) {
       newPassword += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
     }
 
     if (numbers == true) {
-      newPassword += String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+      newPassword += String.fromCharCode(Math.floor(Math.random() * 10) + 49);
     }
 
     if (special == true) {
       const special = "!@#$%^&*+<>/,.";
       newPassword += special[Math.floor(Math.random() * special.length)];
+    }
+
+    if (
+      lowercase == false &&
+      uppercase == false &&
+      numbers == false &&
+      special == false
+    ) {
+      alert("Please start over and chose at least one password option.");
     }
   }
   return newPassword;
@@ -65,8 +76,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// var characters = characters.value;
-// var lowercase = lowercase.value;
-// var uppercase = uppercase.value;
-// var numbers = numbers.value;
-// var special = special.value;
