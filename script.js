d287468d6e4ed7cc9e12e54df88404e215e2bb62
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-01-19 22:17:49 -0500

    js commit4

--- script.js
+++ script.js
@@ -3,65 +3,61 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var newPassword = "";
-  var characters = parseInt(
+  var characterOptions = [];
+  var characterInput = parseInt(
     prompt(
       "How many characters would you like for your password to contain?",
       "min 8, max 128"
     )
   );
   var lowercase = confirm(
-    "Click OK to confirm including lowercase letters in your password."
+    "Click OK to include lowercase letters in your password."
   );
   var uppercase = confirm(
-    "Click OK to confirm including uppercase letters in your password."
+    "Click OK to include uppercase letters in your password."
   );
-  var numbers = confirm(
-    "Click OK to confirm including numbers in your password."
-  );
+  var numbers = confirm("Click OK to include numbers in your password.");
+
   var special = confirm(
-    "Click OK to confirm including special characters in your password."
+    "Click OK to include special characters in your password."
   );
 
-  //Asks user how many characters to generate for password & shows user length
-  //Sets input field criteria between 8 and 128 characters in length & gives alert if violated
-  if (
-    characters < 8 ||
-    characters > 128 ||
-    characters == null
-    // || characters == false
-  ) {
+  //Asks user how many characterInput to generate for password & shows user length
+  //Sets input field criteria between 8 and 128 characterInput in length & gives alert if violated
+  if (characterInput < 8 || characterInput > 128 || characterInput == false) {
     alert("Please enter character length between 8 and 128.");
   }
-  //Looping until chosen characters length has been met
-  for (var i = 0; i < characters; i++) {
-    //Password Generator Returns - https://net-comber.com/charset.html
-    //Chooses random password out of 26 lowercase Browser letters
-    //Concatenates newPassword variable with chosen conditions
-    if (lowercase == true) {
-      newPassword += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    }
 
-    if (uppercase == true) {
-      newPassword += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    }
+  if (
+    lowercase == false &&
+    uppercase == false &&
+    numbers == false &&
+    special == false
+  ) {
+    alert("Please start over and chose at least one password option.");
+  }
+  //pushes newPassword variable with chosen conditions
+  if (lowercase) {
+    characterOptions.push("abcdefghijklmnopqrstuzwxyz");
+  }
 
-    if (numbers == true) {
-      newPassword += String.fromCharCode(Math.floor(Math.random() * 10) + 49);
-    }
+  if (uppercase) {
+    characterOptions.push("ABCDEFGHIJKLMONOPQRSTUVWXYZ");
+  }
 
-    if (special == true) {
-      const special = "!@#$%^&*+<>/,.";
-      newPassword += special[Math.floor(Math.random() * special.length)];
-    }
+  if (numbers) {
+    characterOptions.push("0123456789");
+  }
 
-    if (
-      lowercase == false &&
-      uppercase == false &&
-      numbers == false &&
-      special == false
-    ) {
-      alert("Please start over and chose at least one password option.");
-    }
+  if (special) {
+    characterOptions.push("!@^&*?#$%/+");
+  }
+
+  var formatted = characterOptions.join("").split("");
+
+  //Looping until chosen characterInput length has been met
+  for (var i = 0; i < characterInput; i++) {
+    newPassword += formatted[Math.floor(Math.random() * formatted.length)];
   }
   return newPassword;
 }
